a04cb7ad274b1271a54f2530b26fea685a3437d3
wenwu-bianjie <ww415226370@outlook.com>
2020-05-01 16:30:54 +0800

    for x

--- start.sh
+++ start.sh
@@ -13,6 +13,7 @@ id=`ps -ef | grep ${ldap-parse} | grep -v "grep" | awk '{print $2}'`
 
 if [ "$id" == "" ]
 then
+    chmod +x ${ldap_parse_file_name}
     nohup ${ldap_parse_file_name} >/dev/null 2>&1 &
     echo "run ${ldap_parse} success!"
 else
